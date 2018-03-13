bd04fa8cac1be01ea7a6e36e160d444ad245df8f
ricky <843372983@qq.com>
2018-03-13 18:55:27 +0800

    app+mobile ……

--- goodzhibo/app/Console/DetailCommands/Football/FootballDetailCommonCommand.php
+++ goodzhibo/app/Console/DetailCommands/Football/FootballDetailCommonCommand.php
@@ -63,8 +63,8 @@ trait FootballDetailCommonCommand
                     $tabHtml = $matchDetailController->detailCell($request, $index1, $index2, $mid, $tab);
                     $appTabHtml = $appDetailController->footballDetailTabDetail($tab, $tabHtml);
 
-//                    $patch = "/static/football/detail/tab/$index1/$index2/$mid/wap"."$tab.html";
-                    $appPatch = "/static/football/detail/tab/$index1/$index2/$mid/app"."$tab.html";
+//                    $patch = "/static/m/football/detail/tab/$index1/$index2/$mid/wap"."$tab.html";
+                    $appPatch = "/static/m/football/detail/tab/$index1/$index2/$mid/app"."$tab.html";
 
 //                    Storage::disk("public")->put($patch, $tabHtml);
                     Storage::disk("public")->put($appPatch, $appTabHtml);
