71f449026e3337bc937cf40cde98b4b6998824c2
ricky <843372983@qq.com>
2018-09-17 22:34:43 +0800

    encodes cctv5

--- encodes/app/Http/Controllers/Controller.php
+++ encodes/app/Http/Controllers/Controller.php
@@ -150,7 +150,8 @@ class Controller extends BaseController
         if (starts_with($input_uri, 'http')) {
             if (str_contains($input_uri, '5club.cctv.cn')) {
                 $execs[] = '-user_agent "cctv_app_phone_cctv5"';
-                $execs[] = '-headers "UID:4044A747-5BF0-4465-A894-99E2FEBAC4C1"';
+                $execs[] = '-headers "Referer: api.cctv.cn"';
+                $execs[] = '-headers "UID: 269482797625189"';
             } elseif (str_contains($input_uri, 'http://cctv5')) {
                 $execs[] = '-user_agent "Mozilla / 5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 63.0.3239.84 Safari / 537.36"';
                 $execs[] = '-headers "Referer: http://tv.cctv.com/live/cctv5/"';
