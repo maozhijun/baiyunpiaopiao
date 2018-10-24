6a8f2ab28972033082b771915a5542af31da376f
ricky <843372983@qq.com>
2018-10-24 19:43:39 +0800

    encodes ……

--- encodes/app/Http/Controllers/Pull/PPTVEncodesController.php
+++ encodes/app/Http/Controllers/Pull/PPTVEncodesController.php
@@ -25,7 +25,7 @@ class PPTVEncodesController extends BaseController
     private $userAgentPC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
     private $userAgentApp = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
 
-    private $isDebug = true;
+    private $isDebug = false;
 
     public function __construct()
     {
